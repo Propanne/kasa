2c811365fbcb5480b0fe4240b752b486efe73743
Romain QUADRELLI <romain.quadrelli@smile.fr>
2023-10-04 23:27:18 +0200

    Ajout photo + texte footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,24 @@
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import KasaBlackWhiteSLogo from '../ressources/img/black_white_logo.png';
+
 function Footer() {
     return(
-        <>
-        <p>Footer</p>
-        </>
+        <Container>
+            <Row>
+                <Col xs={12}>
+                    <img
+                            src={KasaBlackWhiteSLogo}
+                            width="150"
+                            height="50"
+                            className="d-inline-block align-top"
+                            alt="Logo Kasa noir et blanc"
+                    />
+                </Col>
+                <Col xs={12}>© 2020 Kasa. All rights reserved</Col>
+            </Row>
+        </Container>
     );
 }
 
