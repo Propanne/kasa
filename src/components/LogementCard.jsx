0bd300d31249b19e0c6248ab6f307198e9c8ab44
Romain QUADRELLI <romain.quadrelli@smile.fr>
2023-10-11 23:20:21 +0200

    Add linear-gradient to background logement card

--- src/components/LogementCard.jsx
+++ src/components/LogementCard.jsx
@@ -1,6 +1,6 @@
 function LogementCard(props) {
     return(
-        <div className="logementCard" style={{backgroundImage: "url("+props.cover+")"}}>
+        <div className="logementCard" style={{backgroundImage: "linear-gradient(180deg, rgba(255, 255, 255, 0) 0%, rgba(0, 0, 0, 0.5) 100%),url("+props.cover+")"}}>
             <div className="logementCard-desc-container"><p className="logementCard-desc-text">{props.title}</p></div>
         </div>
     );
